6b28bb4419f5297d02c2e7d08f1fb80e64f4cb0c
martin <msrroschi@gmail.com>
2021-03-05 20:52:14 +0000

    Get appointments into calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -65,26 +65,26 @@
         :activator="selectedElement"
         offset-x
       >
-        <v-card color="grey lighten-4" min-width="350px" flat>
+        <v-card min-width="350px" max-height="500px" flat v-if="selectedOpen">
           <v-toolbar :color="selectedEvent.color" dark>
-            <v-btn icon>
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
             <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
+            <v-toolbar-title v-html="selectedEvent.startTime"></v-toolbar-title>
           </v-toolbar>
           <v-card-text>
-            <span v-html="selectedEvent.details"> </span>
+            <h2 class="mt-4 mb-2">
+              {{ selectedEvent.patient.firstName }}
+              {{ selectedEvent.patient.lastName }}
+            </h2>
+            <v-divider></v-divider>
+            <p class="mt-2 mb-2">Piezas: {{ selectedEvent.pieces }}</p>
+            <h3 class="mt-2 mb-2">Doctor: {{ selectedEvent.employee }}</h3>
+            <p class="mt-2">Otros: {{ selectedEvent.otherEmployees.join() }}</p>
           </v-card-text>
           <v-card-actions>
+            <v-spacer></v-spacer>
             <v-btn text color="secondary" @click="selectedOpen = false">
-              Cancel
+              Ver más
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -135,9 +135,6 @@ export default {
     this.$refs.calendar.checkChange()
   },
   methods: {
-    vuetify() {
-      console.log(this.$vuetify.breakpoint.smAndDown)
-    },
     viewDay({ date }) {
       this.focus = date
       this.type = 'day'
@@ -156,12 +153,9 @@ export default {
     },
     showEvent({ nativeEvent, event }) {
       const open = () => {
+        this.getAppointment(event)
         this.selectedEvent = event
         this.selectedElement = nativeEvent.target
-        console.log(this.selectedEvent)
-        setTimeout(() => {
-          this.selectedOpen = true
-        }, 10)
       }
 
       if (this.selectedOpen) {
@@ -173,57 +167,54 @@ export default {
 
       nativeEvent.stopPropagation()
     },
-    updateRange(/*{ start, end }*/) {
+    updateRange() {
       const events = []
-
-      // const min = new Date(`${start.date}T00:00:00`)
-      // const max = new Date(`${end.date}T23:59:59`)
-      // const days = (max.getTime() - min.getTime()) / 86400000
-      // const eventCount = this.rnd(days, days + 20)
 
       AppointmentService.getAppointments()
         .then(appointments => {
           appointments.data.forEach(appointment => {
-            appointment.employees.forEach((employee, i) => {
+            let startHour = new Date(appointment.start).getHours()
+            let startMinute = new Date(appointment.start).getMinutes()
+
+            let endHour = new Date(appointment.end).getHours()
+            let endMinute = new Date(appointment.end).getMinutes()
+
+            appointment.employees.forEach(employee => {
               events.push({
                 name: appointment.intervention,
                 start: new Date(appointment.start),
+                startTime: `${startHour}:${startMinute}`,
                 end: new Date(appointment.end),
-                color: appointment.employees[i].color,
+                endTime: `${endHour}:${endMinute}`,
+                color: employee.color,
                 timed: true,
-                info: appointment,
-                idx: i
+                employee: `${employee.firstName} ${employee.lastName}`,
+                otherEmployees: this.getOtherEmployees(appointment, employee),
+                appointmentId: appointment._id
               })
             })
           })
         })
         .catch(err => console.log(err))
 
-      // for (let i = 0; i < eventCount; i++) {
-      //   const allDay = this.rnd(0, 3) === 0
-      //   const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-      //   const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-      //   const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-      //   const second = new Date(first.getTime() + secondTimestamp)
-
-      //   events.push({
-      //     name: this.names[this.rnd(0, this.names.length - 1)],
-      //     start: first,
-      //     end: second,
-      //     color: this.colors[this.rnd(0, this.colors.length - 1)],
-      //     timed: !allDay
-      //   })
-      // }
-
       this.events = events
-      console.log(this.events)
-      this.events.push({
-        name: 'Meeting',
-        start: '2021-03-06 19:30',
-        end: '2021-03-06 20:30',
-        color: 'teal',
-        timed: true
-      })
+    },
+    getOtherEmployees(appointment, employee) {
+      return appointment.employees
+        .filter(otherEmployee => otherEmployee !== employee)
+        .map(
+          otherEmployee =>
+            `${otherEmployee.firstName} ${otherEmployee.lastName}`
+        )
+    },
+    getAppointment(event) {
+      AppointmentService.getAppointmentById(event.appointmentId)
+        .then(appointment => {
+          event.patient = appointment.data.patient
+          event.pieces = appointment.data.piece.join()
+          this.selectedOpen = true
+        })
+        .catch(err => console.log(err))
     },
     rnd(a, b) {
       return Math.floor((b - a + 1) * Math.random()) + a
--- src/services/appointmentService.js
+++ src/services/appointmentService.js
@@ -3,5 +3,8 @@ import { api } from '../services/apiurl'
 export default {
   async getAppointments() {
     return await api.get('/appointments')
+  },
+  async getAppointmentById(appointmentId) {
+    return await api.get(`/appointments/${appointmentId}`)
   }
 }
