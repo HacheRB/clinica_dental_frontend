62ff69a94fdf633a25a5b1d1aae7fd991042aaa6
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-03-09 10:11:31 +0000

    Add gradient to LandingHero

--- src/components/LandingHero.vue
+++ src/components/LandingHero.vue
@@ -2,45 +2,64 @@
   <v-row>
     <v-img
       :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+      :aspect-ratio="16 / 9"
+      gradient="to top right, rgba(0,0,0,0.6), rgba(0,0,0,0.6)"
       src="https://images.unsplash.com/photo-1598256989800-fe5f95da9787?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
     >
       <v-theme-provider dark>
         <v-container fill-height>
-          <v-row align="center" class="white--text mx-auto" justify="center">
-            <v-col class="white--text text-center" cols="12" tag="h1">
-              <span
-                :class="[
-                  $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2'
-                ]"
-                class="font-weight-light"
-              >
+          <v-row
+            align="center"
+            class=" d-flex white--text mx-auto"
+            justify="center"
+          >
+            <v-col
+              class="d-flex flex-row justify-center align-center white--text text-center"
+              cols="12"
+              md="12"
+              lg="6"
+              xl="4"
+            >
+              <h1>
                 Clinica Marrero
-              </span>
+              </h1>
 
               <br />
-
-              <span
-                :class="[
-                  $vuetify.breakpoint.smAndDown ? 'display-3' : 'display-4'
-                ]"
-                class="font-weight-black"
-              >
-                demo
-              </span>
             </v-col>
-
-            <v-btn
-              class="align-self-end"
-              fab
-              outlined
-              @click="$vuetify.goTo('#about-me')"
+            <v-col
+              class="d-flex flex-row justify-center align-center white--text text-center"
+              cols="12"
+              sm="8"
+              md="8"
+              lg="6"
+              xl="4"
             >
-              <v-icon>mdi-chevron-double-down</v-icon>
-            </v-btn>
+              <p
+                class="text-left text-body-2 text-sm-body-2 text-md-body-1 text-lg-body-1 text-xl-body-1 mb-2"
+              >
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                quis, sem. Nulla consequat massa quis enim. Donec pede justo,
+                fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                rhoncus ut, imperdiet a
+              </p>
+            </v-col>
+            <v-col
+              class="white--text text-center"
+              cols="12"
+              md="6"
+              lg="6"
+              xl="12"
+            >
+              <v-btn fab outlined @click="$vuetify.goTo('#about-me')">
+                <v-icon>mdi-chevron-double-down</v-icon>
+              </v-btn>
+            </v-col>
           </v-row>
         </v-container>
       </v-theme-provider>
-      <div class="fill-height image-gradient"></div>
     </v-img>
   </v-row>
 </template>
@@ -50,6 +69,13 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.bottom-gradient {
+  background-image: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.4) 0%,
+    transparent 72px
+  );
+}
 .image-gradient {
   background-image: repeating-linear-gradient(
     -45deg,
