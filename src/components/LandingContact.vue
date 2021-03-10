889d0b474f595df6b2a203f9f2cf6d0cd7daa92b
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-03-10 12:36:53 +0000

    Finish Landing

---
+++ src/components/LandingContact.vue
@@ -0,0 +1,156 @@
+<template>
+  <v-container id="contact" fluid class="contact px-0 px-sm-15">
+    <div class="mt-3 py-15 contact"></div>
+
+    <div
+      class=" text-xs-body-2 display-2 text-center text-uppercase font-weight-bold mb-2"
+    >
+      Contáctanos
+    </div>
+    <v-responsive class="mx-auto mb-8" width="150">
+      <v-divider class="mb-1"></v-divider>
+    </v-responsive>
+    <v-row class="d-flex justify-center" cols="12" lg="8" xl="6">
+      <v-col text-left cols="12" lg="8" xl="6">
+        <p class=" px-5 pb-5 px-sm-0" text-left cols="12" lg="8" xl="6">
+          Si tiene alguna duda, necesita realizar una consulta, reservar hora
+          para su próxima cita o su primera visita, puede ponerse en contacto
+          con nosotros mediante el siguiente formulario de contacto. Le
+          responderemos con la mayor brevedad que nos sea posible.
+        </p>
+      </v-col>
+    </v-row>
+    <v-row class="d-flex justify-center" cols="12" lg="8" xl="6">
+      <v-col text-left cols="12" lg="8" xl="6">
+        <v-card
+          outlined
+          class="d-flex flex-column justify-center"
+          cols="12"
+          sm="6"
+        >
+          <v-card-text>
+            <v-form ref="form">
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6">
+                    <v-text-field
+                      color="teal lighten-1"
+                      v-model="fullName"
+                      label="Nombre y Apellidos *"
+                      required
+                      @change="change"
+                      :rules="fullNameRules"
+                    >
+                    </v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-text-field
+                      color="teal lighten-1"
+                      v-model="email"
+                      :rules="emailRules"
+                      label="Email *"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-text-field
+                      color="teal lighten-1"
+                      v-model="phone"
+                      :rules="phoneRules"
+                      label="Teléfono de contacto *"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <h4>Motivo de la consulta</h4>
+                    <v-select
+                      color="teal lighten-1"
+                      :items="opciones"
+                      label="Seleccione un motivo"
+                      :rules="bloodTypeRules"
+                      v-model="motivo"
+                      item-color="teal lighten-1"
+                      solo
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-textarea
+                      color="teal lighten-1"
+                      name="Descripción"
+                      label="Descripción"
+                      outlined
+                      filled
+                      auto-grow
+                      v-model="description"
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+            <small>*indica campos requeridos</small>
+          </v-card-text>
+          <v-card-actions class="d-flex justify-center">
+            <v-btn
+              color="teal darken-2 white--text mb-2"
+              @click.prevent="sendForm"
+            >
+              Solicitar información
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="d-flex flex-col pt-5"> </v-row>
+    <div class="py-15 contact"></div>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'LandingContact',
+  data() {
+    return {
+      fullName: '',
+      description: '',
+      email: '',
+      phone: '',
+      motivo: '',
+      opciones: [
+        'Solicitar Información',
+        'Solicitar Presupuesto',
+        'Pedir Cita'
+      ],
+      fullNameRules: [
+        v => !!v || 'First name is required',
+        v => v.length <= 30 || 'First name must be less than 30 characters'
+      ],
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v =>
+          /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/.test(
+            v
+          ) || 'E-mail must be valid'
+      ],
+      phoneRules: [
+        v => !!v || 'Mobilephone is required',
+        v => /^[679][0-9]{8}$/.test(v) || 'Mobilephone must be valid'
+      ],
+      methods: {
+        sendForm() {
+          if (!this.$refs.form.validate()) {
+            console.log('no pasa las validaciones')
+            return
+          }
+          console.log('Se envia el formulario')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  background-color: #e0f2f1;
+}
+</style>
--- src/components/LandingTeam.vue
+++ src/components/LandingTeam.vue
@@ -1,15 +1,17 @@
 <template>
   <v-container id="team" fluid class="text-center team px-0 ">
-    <div class="mt-3 py-14 team"></div>
+    <div class="mt-3 py-15 team"></div>
 
-    <h2 class="display-2 font-weight-bold mb-3">EQUIPO</h2>
+    <h2 class="display-2 text-uppercase font-weight-bold mb-2">Equipo</h2>
 
-    <v-responsive class="mx-auto mb-8" width="175">
+    <v-responsive class="mx-auto mb-8" width="150">
       <v-divider class="mb-1"></v-divider>
     </v-responsive>
     <v-row class="d-flex flex-col pt-5">
-      <!-- Empleado 1 -->
+      <!-- Empleado -->
       <v-col
+        v-for="(employee, idx) in employees"
+        :key="idx"
         class="d-flex flex-col justify-center pa-5"
         cols="12"
         sm="6"
@@ -27,106 +29,10 @@
             height="200px"
             width="200px"
             hover
-            src="https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80"
+            :src="employee.img"
           ></v-img>
-          <v-card-title class="mt-5">Pepita Grilla</v-card-title>
-          <v-card-subtitle> Dentista</v-card-subtitle>
-          <v-card-text>
-            <p class="no-overflow">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <!-- Empleado 2 -->
-      <v-col
-        class="d-flex flex-col justify-center pa-5"
-        cols="12"
-        sm="6"
-        md="6"
-        lg="3"
-        min-height="100vh"
-      >
-        <v-card
-          outlined
-          color="transparent"
-          class="d-flex flex-column justify-center align-center"
-        >
-          <v-img
-            class="rounded-circle"
-            height="200px"
-            width="200px"
-            hover
-            src="https://images.unsplash.com/photo-1567568443997-19f025355d52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-          ></v-img>
-          <v-card-title class="mt-5">Pepito Grillo</v-card-title>
-          <v-card-subtitle> Dentista</v-card-subtitle>
-          <v-card-text>
-            <p class="no-overflow">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <!-- Empleado 3 -->
-      <v-col
-        class="d-flex flex-col justify-center pa-5"
-        cols="12"
-        sm="6"
-        md="6"
-        lg="3"
-        min-height="100vh"
-      >
-        <v-card
-          outlined
-          color="transparent"
-          class="d-flex flex-column justify-center align-center"
-        >
-          <v-img
-            class="rounded-circle"
-            height="200px"
-            width="200px"
-            hover
-            src="https://images.unsplash.com/photo-1588776813677-77aaf5595b83?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
-          ></v-img>
-          <v-card-title class="mt-5">Pepito Grillo</v-card-title>
-          <v-card-subtitle>Auxiliar</v-card-subtitle>
-          <v-card-text>
-            <p class="no-overflow">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <!-- Empleado 4 -->
-      <v-col
-        class="d-flex flex-col justify-center pa-5"
-        cols="12"
-        sm="6"
-        md="6"
-        lg="3"
-        min-height="100vh"
-      >
-        <v-card
-          outlined
-          color="transparent"
-          class="d-flex flex-column justify-center align-center"
-        >
-          <v-img
-            class="rounded-circle"
-            height="200px"
-            width="200px"
-            hover
-            src="https://images.unsplash.com/photo-1527613426441-4da17471b66d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1935&q=80"
-          ></v-img>
-          <v-card-title class="mt-5">Pepita Grilla</v-card-title>
-          <v-card-subtitle>Auxiliar</v-card-subtitle>
+          <v-card-title class="mt-5">{{ employee.name }}</v-card-title>
+          <v-card-subtitle>{{ employee.title }}</v-card-subtitle>
           <v-card-text>
             <p class="no-overflow">
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
@@ -137,12 +43,46 @@
         </v-card>
       </v-col>
     </v-row>
-    <div class="py-12 team"></div>
+    <div class="py-15 team"></div>
   </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'LandingTeam',
+  data() {
+    return {
+      employeeDescription:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes',
+      employees: [
+        {
+          name: 'Pepita Grillo',
+          title: 'Dentista',
+          img:
+            'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          name: 'Pepito Grillo',
+          title: 'Dentista',
+          img:
+            'https://images.unsplash.com/photo-1567568443997-19f025355d52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+        },
+        {
+          name: 'Pepito Grillo',
+          title: 'Auxiliar',
+          img:
+            'https://images.unsplash.com/photo-1588776813677-77aaf5595b83?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+        },
+        {
+          name: 'Pepita Grilla',
+          title: 'Auxiliar',
+          img:
+            'https://images.unsplash.com/photo-1527613426441-4da17471b66d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1935&q=80'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
