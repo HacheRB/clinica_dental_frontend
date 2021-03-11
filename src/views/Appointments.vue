db28e71d2182f6b62105b6ae89a688bdd97e16f8
martin <msrroschi@gmail.com>
2021-03-11 21:21:42 +0000

    Chaneg Appointment info to component

--- src/views/Appointments.vue
+++ src/views/Appointments.vue
@@ -33,7 +33,7 @@
             :options.sync="options"
             :server-items-length="totalAppointments"
             :search="search"
-            @click:row="displayAppointment"
+            @click:row="sendAppointment"
           >
             <template v-slot:item.action="{ item }">
               <v-btn @click.stop="deleteAppointment(item)" icon>
@@ -44,169 +44,26 @@
         </v-card>
       </v-col>
     </v-row>
-    <v-navigation-drawer
-      right
-      v-model="drawer"
-      app
-      v-if="selectedAppointment"
-      width="500px"
-      color="teal darken-2"
-    >
-      <v-card color="teal darken-2" dark outlined>
-        <v-card-title>
-          <v-col>
-            {{ selectedAppointment.patient.firstName }}
-            {{ selectedAppointment.patient.lastName }}
-          </v-col>
-          <v-col class="d-flex justify-end">
-            <v-btn @click="drawer = false" color="white" outlined
-              ><v-icon> mdi-close</v-icon></v-btn
-            >
-          </v-col>
-        </v-card-title>
-        <v-card-text>
-          <v-sheet light height="100%" rounded class="pa-3">
-            <v-container>
-              <v-row>
-                <v-col class="teal--text d-flex align-center justify-start">
-                  {{ selectedAppointment.start }} |
-                  {{ selectedAppointment.end.slice(-5) }}
-                </v-col>
-                <v-col>
-                  <v-row class="d-flex justify-end">
-                    <v-btn
-                      dark
-                      :color="
-                        !this.selectedAppointment.treatment.finished
-                          ? 'red'
-                          : 'teal'
-                      "
-                      x-small
-                      @click="finishTreatment"
-                      :key="finishTreatmentBtnKey"
-                      >{{
-                        !this.selectedAppointment.treatment.finished
-                          ? 'Finalizar Tratamiento'
-                          : 'Reanudar Tratamiento'
-                      }}</v-btn
-                    >
-                  </v-row>
-                  <v-row class="d-flex justify-end">
-                    <v-btn
-                      class="mt-3"
-                      dark
-                      :color="
-                        !this.selectedAppointment.finished ? 'red' : 'teal'
-                      "
-                      x-small
-                      @click="finishAppointment"
-                      :key="finishAppointmentBtnKey"
-                      >{{
-                        !this.selectedAppointment.finished
-                          ? 'Finalizar Cita'
-                          : 'Reanudar Cita'
-                      }}</v-btn
-                    >
-                  </v-row>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <v-list>
-                    <v-list-item
-                      v-for="(employee, idx) in selectedAppointment.employees"
-                      :key="idx"
-                    >
-                      <v-btn
-                        :color="employee.color"
-                        x-small
-                        class="mr-3"
-                      ></v-btn>
-                      {{ employee.firstName }}
-                      {{ employee.lastName }}
-                    </v-list-item>
-                  </v-list>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <v-textarea
-                    v-model="changedAppointment.observations"
-                    outlined
-                    label="Motivos y Observaciones"
-                    @change="changeAppointment"
-                  >
-                  </v-textarea>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <v-row>
-                    <v-col>
-                      <v-text-field
-                        v-model="changedAppointment.intervention"
-                        outlined
-                        label="Tipo de intervención"
-                        @change="changeAppointment"
-                      >
-                      </v-text-field>
-                    </v-col>
-                    <v-col>
-                      <v-text-field
-                        v-model="
-                          changedAppointment.treatment.interventionSubType
-                        "
-                        outlined
-                        label="Subtipo de intervención"
-                        @change="changeAppointment"
-                      >
-                      </v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <v-textarea
-                    v-model="changedAppointment.medicines"
-                    label="Medicinas"
-                    outlined
-                    @change="changeAppointment"
-                  >
-                  </v-textarea>
-                </v-col>
-              </v-row>
-              <v-row v-if="showUpdateButton">
-                <v-col class="d-flex justify-center">
-                  <v-btn small dark color="teal" @click="updateAppointment"
-                    >Guardar cambios</v-btn
-                  >
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-sheet>
-        </v-card-text>
-      </v-card>
-    </v-navigation-drawer>
+    <AppointmentInfo
+      :appointment="selectedAppointment"
+      @resetSelectedAppointment="selectedAppointment = null"
+    />
   </v-container>
 </template>
 
 <script>
 import AppointmentService from '../services/appointmentService'
-import TreatmentService from '../services/treatmentService'
+import AppointmentInfo from '../components/AppointmentInfo'
 
 export default {
   name: 'Appointments',
+  components: {
+    AppointmentInfo
+  },
   data: () => ({
     appointments: [],
     selectedAppointment: null,
     changedAppointment: {},
-    drawer: false,
-    showUpdateButton: false,
-    finishTreatmentBtnText: '',
-    finishTreatmentBtnKey: true,
-    finishAppointmentBtnKey: true,
-    updateDataTable: true,
     search: '',
     totalAppointments: 0,
     options: {},
@@ -255,14 +112,9 @@ export default {
     }
   },
   methods: {
-    displayAppointment: function(appointment) {
-      AppointmentService.getAppointmentById(appointment._id)
-        .then(appointment => {
-          this.selectedAppointment = appointment.data
-          this.drawer = true
-          Object.assign(this.changedAppointment, this.selectedAppointment)
-        })
-        .catch(err => console.log(err))
+    sendAppointment: function(appointment) {
+      this.selectedAppointment = appointment
+      console.log(this.selectedAppointment)
     },
     doSearch: function() {
       console.log(
@@ -298,56 +150,6 @@ export default {
               }
             })
           }
-        })
-        .catch(err => console.log(err))
-    },
-    updateAppointment: function() {
-      console.log(this.changedAppointment)
-      AppointmentService.updateAppointment(this.changedAppointment)
-        .then(appointment => {
-          console.log(appointment)
-          this.selectedAppointment = appointment.data
-          this.showUpdateButton = false
-          this.finishAppointmentBtnKey = !this.finishAppointmentBtnKey
-        })
-        .catch(err => console.log(err))
-    },
-    finishTreatment: function() {
-      this.selectedAppointment.treatment.finished = !this.selectedAppointment
-        .treatment.finished
-      TreatmentService.updateTreatment(this.selectedAppointment.treatment)
-        .then(treatment => {
-          this.selectedAppointment.treatment = treatment.data
-          this.finishTreatmentBtnKey = !this.finishTreatmentBtnKey
-        })
-        .catch(err => console.log(err))
-    },
-    finishAppointment: function() {
-      this.changedAppointment.finished = !this.changedAppointment.finished
-      this.updateAppointment()
-    },
-    changeAppointment: function() {
-      if (
-        this.selectedAppointment.observations !==
-          this.changedAppointment.observations ||
-        this.selectedAppointment.intervention !==
-          this.changedAppointment.intervention ||
-        this.selectedAppointment.treatment.interventionSubType !==
-          this.changedAppointment.treatment.interventionSubType ||
-        this.selectedAppointment.medicines !==
-          this.changedAppointment.medicines ||
-        this.selectedAppointment.finished !== this.changedAppointment.finished
-      ) {
-        this.showUpdateButton = true
-      } else {
-        this.showUpdateButton = false
-      }
-    },
-    deleteAppointment: function(appointment) {
-      console.log(appointment)
-      AppointmentService.deleteAppointment(appointment)
-        .then(response => {
-          console.log(response.data)
         })
         .catch(err => console.log(err))
     }
