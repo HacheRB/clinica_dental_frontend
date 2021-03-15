4f9fce1ef8d2eb0ecba26494a777fde580449e60
martin <msrroschi@gmail.com>
2021-03-15 17:38:03 +0000

    Add more style to PatientMedicalHistory

--- src/views/PatientMedicalHistory.vue
+++ src/views/PatientMedicalHistory.vue
@@ -33,7 +33,7 @@
             >
               <v-row no-gutters>
                 <v-col cols="6" md="4">
-                  {{ treatment.appointments[0].start }}
+                  Fecha de la última cita: {{ treatment.appointments[0].start }}
                 </v-col>
                 <v-col cols="6" class="text--secondary">
                   <v-fade-transition leave-absolute>
@@ -41,7 +41,7 @@
 
                     <v-row v-else no-gutters style="width: 100%">
                       <v-col cols="6" md="4">
-                        {{ treatment.appointments[0].intervention }}
+                        {{ treatment.intervention }}
                       </v-col>
                       <v-col v-if="$vuetify.breakpoint.mdAndUp" cols="6" md="4">
                         {{
@@ -63,61 +63,13 @@
                   :key="idx"
                   cols="12"
                   sm="6"
-                  md="4"
+                  :lg="selectedAppointment ? 6 : 4"
                 >
-                  <v-card
-                    color="teal darken-2"
-                    @click="selectedAppointment = appointment"
-                    class="d-flex flex-column pb-5 white--text"
-                  >
-                    <v-row class="d-flex  align-baseline px-5 mt-2">
-                      <v-col cols="12" sm="6">
-                        <p class="title align-end">
-                          {{ treatment.interventionSubtype }}
-                        </p>
-                      </v-col>
-                      <v-col cols="12" sm="6" class="d-flex justify-end">
-                        <p class="title">
-                          {{
-                            appointment.start + '-' + appointment.end.slice(-5)
-                          }}
-                        </p>
-                      </v-col>
-                    </v-row>
-                    <v-row class=" d-flex flex-column justify-center px-5 mt-5">
-                      <v-col>
-                        <span class="subtitle-1">
-                          Pieces :
-                        </span>
-                        <span
-                          class="subtitle-1"
-                          v-for="(piece, idx) in appointment.pieces"
-                          :key="idx"
-                        >
-                          {{ piece }}
-                        </span>
-                      </v-col>
-                      <v-col>
-                        <span class="font-weight-medium pa-0">
-                          Empleados:
-                        </span>
-                        <span
-                          class="body-1 pa-0"
-                          v-for="(employee, idx) in appointment.employees"
-                          :key="idx"
-                        >
-                          {{
-                            idx !== appointment.employees.length - 1
-                              ? employee.firstName +
-                                ' ' +
-                                employee.lastName +
-                                ', '
-                              : employee.firstName + ' ' + employee.lastName
-                          }}</span
-                        >
-                      </v-col>
-                    </v-row>
-                  </v-card>
+                  <AppointmentCard
+                    :treatment="treatment"
+                    :appointment="appointment"
+                    @selectedAppointment="selectedAppointment = appointment"
+                  />
                 </v-col>
               </v-row>
             </v-expansion-panel-content>
@@ -136,12 +88,14 @@
 import PatientService from '../services/patientService'
 import TreatmentService from '../services/treatmentService'
 import AppointmentInfo from '../components/AppointmentInfo'
+import AppointmentCard from '../components/AppointmentCard'
 
 export default {
   name: 'PatientMedicalHistory',
   props: { patientId: String },
   components: {
-    AppointmentInfo
+    AppointmentInfo,
+    AppointmentCard
   },
   data() {
     return {
